14a74b35309205f33062e4113c3c42d4d3efa546
Maksim Kanev <maks_kv@yahoo.com>
2025-07-02 17:13:27 +0300

    Integrated quiz creation/edit form into Admin Panel

--- src/views/AdminPanel.vue
+++ src/views/AdminPanel.vue
@@ -1,25 +1,53 @@
 <template>
   <div class="max-w-3xl mx-auto p-8">
     <h1 class="text-2xl font-bold mb-6">Admin Panel</h1>
-    <AdminQuizList @create="onCreate" @edit="onEdit" @delete="onDelete" />
+    <AdminQuizList v-if="!showForm" @create="onCreate" @edit="onEdit" @delete="onDelete" />
+    <AdminQuizForm
+      v-else
+      :model-value="editingQuiz"
+      @save="onSave"
+      @cancel="onCancel"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
+import { useQuizStore } from '../store/quiz'
 import AdminQuizList from '../components/AdminQuizList.vue'
+import AdminQuizForm from '../components/AdminQuizForm.vue'
+import type { Quiz } from '../types/quiz'
+
+const quizStore = useQuizStore()
+const showForm = ref(false)
+const editingQuiz = ref<Quiz | null>(null)
 
 function onCreate() {
-  // TODO: Show quiz creation form
-  alert('Create new quiz (form coming soon)')
+  editingQuiz.value = null
+  showForm.value = true
 }
 
-function onEdit(quiz: any) {
-  // TODO: Show quiz edit form
-  alert(`Edit quiz: ${quiz.title} (form coming soon)`)
+function onEdit(quiz: Quiz) {
+  editingQuiz.value = quiz
+  showForm.value = true
 }
 
 function onDelete(id: string) {
-  // TODO: Confirm and delete quiz
-  alert(`Delete quiz with id: ${id} (delete logic coming soon)`)
+  if (confirm('Are you sure you want to delete this quiz?')) {
+    quizStore.deleteQuiz(id)
+  }
+}
+
+function onSave(quiz: Quiz) {
+  if (editingQuiz.value) {
+    quizStore.updateQuiz(quiz)
+  } else {
+    quizStore.addQuiz({ ...quiz, id: Date.now().toString(), questions: quiz.questions || [] })
+  }
+  showForm.value = false
+}
+
+function onCancel() {
+  showForm.value = false
 }
 </script> 
