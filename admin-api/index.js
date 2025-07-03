1b9b7da059694976055259e9267fd77e496d4cfd
Maksim Kanev <maks_kv@yahoo.com>
2025-07-03 19:20:47 +0300

    brought back tls value

--- admin-api/index.js
+++ admin-api/index.js
@@ -78,6 +78,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 })
 
 // Checks for valid admin token and privileges
